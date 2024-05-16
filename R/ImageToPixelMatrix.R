d33fb9e30f8ff877527a60b96172029669d8b7c9
CarissaGervasi-NOAA <carissa.gervasi@noaa.gov>
2024-05-16 16:43:11 -0400

    fixing arrow image

--- R/ImageToPixelMatrix.R
+++ R/ImageToPixelMatrix.R
@@ -5,13 +5,25 @@
 library(magick)
 
 # Step 1: Read the image
-image <- image_read("data/arrowSide2.svg")  # Replace with your image path
+image <- image_read("data/arrowSide2.png")  # Replace with your image path
 
 # Step 2: Convert the image to RGB matrix
 image_data <- image_data(image, channels = "rgb")
-image_matrix <- as.numeric(image_data)
+arrowSide <- as.numeric(image_data)
+
+dim(arrowSide)
+dim(arrowDown)
+
+# Reshape the 3D array into a 2D matrix
+# One approach is to collapse the third dimension (z) into columns
+
+dims <- dim(arrowSide)
+arrowSide <- matrix(arrowSide, nrow = dims[1], ncol = dims[2] * dims[3])
+
+# Check the resulting matrix
+print(dim(arrowSide))
 
 # Step 3: Save the matrix as a .rds file
-save(image_matrix, file = "data/arrowSide.rda")
+save(arrowSide, file = "data/arrowSide.rda")
 
 
